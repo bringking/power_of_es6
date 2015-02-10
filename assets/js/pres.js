fa1a491bee7efb6987917028da4f45e7688f81d9
Charlie <bringking@gmail.com>
2015-02-10 12:14:28 -0700

    fixed editors for mobile

--- assets/js/pres.js
+++ assets/js/pres.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function( event ) {
 
+    var mobilecheck = function() {
+        var check = false;
+        (function( a, b ) {
+            if ( /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)) )check = true
+        })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    };
+
     var currentInputEditor, currentOutputEditor;
 
     //attach click handler to expanders
@@ -13,19 +21,19 @@ document.addEventListener("DOMContentLoaded", function( event ) {
 
             //find the other header
             var otherHeader;
-            if (parentHeader.hasClass("editor-input-header")) {
-                 otherHeader = $(parentHeader.siblings(".editor-output-header"));
+            if ( parentHeader.hasClass("editor-input-header") ) {
+                otherHeader = $(parentHeader.siblings(".editor-output-header"));
             } else {
-                 otherHeader = $(parentHeader.siblings(".editor-input-header"));
+                otherHeader = $(parentHeader.siblings(".editor-input-header"));
             }
 
             //add classes to editor
-            if (editor.classList.contains("expanded")) {
-               editor.classList.remove("expanded");
-               otherHeader.css({visibility : "visible"});
+            if ( editor.classList.contains("expanded") ) {
+                editor.classList.remove("expanded");
+                otherHeader.css({visibility: "visible"});
             } else {
                 editor.classList.add("expanded");
-                otherHeader.css({visibility : "hidden"});
+                otherHeader.css({visibility: "hidden"});
             }
 
             //trigger resize
@@ -57,10 +65,15 @@ document.addEventListener("DOMContentLoaded", function( event ) {
             }
             },
             {
-                src: 'assets/js/highlight/highlight.js', async: true, condition: function() {
+                src: 'assets/js/highlight/highlight.js', condition: function() {
                 return !!document.querySelector('pre code');
             }, callback: function() {
+                window.hljs = hljs;
+                hljs.configure({
+                    languages: ["javascript"]
+                });
                 hljs.initHighlightingOnLoad();
+
             }
             }
 
@@ -73,7 +86,7 @@ document.addEventListener("DOMContentLoaded", function( event ) {
     function transformText( input, opts ) {
         var parsed = "";
         try {
-            parsed = to5.transform(input,opts).code;
+            parsed = to5.transform(input, opts).code;
         }
         catch ( err ) {
             //console.error(err);
@@ -90,6 +103,9 @@ document.addEventListener("DOMContentLoaded", function( event ) {
      */
     function setupEditors( slideElement ) {
 
+        if ( !slideElement )
+            return;
+
         //get the input editor
         var foundInput = slideElement.querySelectorAll(".editor-input")[0];
 
@@ -103,6 +119,21 @@ document.addEventListener("DOMContentLoaded", function( event ) {
 
         //attach the ace editor
         if ( foundInput ) {
+            //don't use ace on mobile
+            if ( mobilecheck() ) {
+                var inputCode = foundInput.innerHTML;
+                if ( inputCode.indexOf('<pre') === -1 ) {
+
+                    var inputPre = $('<pre class="full-height"><code class="hljs javascript">' + inputCode + '</code></pre>');
+                    foundInput.innerHTML = "";
+                    $(foundInput).append(inputPre);
+                    hljs.highlightBlock(inputPre[0]);
+
+                }
+
+                return;
+            }
+
             input = ace.edit(foundInput);
             currentInputEditor = input;
             input.setTheme("ace/theme/monokai");
@@ -116,6 +147,20 @@ document.addEventListener("DOMContentLoaded", function( event ) {
         }
         //attach the ace editor
         if ( foundOutput ) {
+
+            //don't use ace on mobile
+            if ( mobilecheck() ) {
+                var outputCode = foundOutput.innerHTML;
+                if ( outputCode.indexOf('<pre') === -1 ) {
+                    var outputPre = $('<pre class="full-height"><code class="hljs javascript">' + outputCode + '</code></pre>');
+                    foundOutput.innerHTML = "";
+                    $(foundOutput).append(outputPre);
+                    hljs.highlightBlock(outputPre[0]);
+                }
+
+                return;
+            }
+
             output = ace.edit(foundOutput);
             currentOutputEditor = output;
             output.setTheme("ace/theme/monokai");
@@ -126,12 +171,11 @@ document.addEventListener("DOMContentLoaded", function( event ) {
             output.renderer.setShowGutter(false);
             output.setHighlightActiveLine(false);
             //set the initial value
-            if (foundInput.classList.contains("amd")) {
-                output.setValue(transformText(input.getValue(),{modules:"amd"}), 0);
+            if ( foundInput.classList.contains("amd") ) {
+                output.setValue(transformText(input.getValue(), {modules: "amd"}), 0);
             } else {
-                output.setValue(transformText(input.getValue(),{modules:"common"}), 0);
+                output.setValue(transformText(input.getValue(), {modules: "common"}), 0);
             }
-
 
         }
         //attach the ace editor
@@ -155,17 +199,16 @@ document.addEventListener("DOMContentLoaded", function( event ) {
 
                 //set the initial value
                 var opts;
-                if (foundInput.classList.contains("amd")) {
-                    opts = {modules:"amd"};
+                if ( foundInput.classList.contains("amd") ) {
+                    opts = {modules: "amd"};
                 } else {
-                    opts = {modules:"common"};
+                    opts = {modules: "common"};
                 }
-
 
                 //get the ES5 transformed value
                 newVal = native
                     ? input.getValue()
-                    : transformText(input.getValue(),opts);
+                    : transformText(input.getValue(), opts);
 
                 if ( newVal ) {
                     //if there is an output window, set the value
